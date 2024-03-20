84d7b213c4d191486742f5e276635bddc634b81c
abhi <abhi@techno.co>
2024-03-20 22:41:24 +0530

    add login feature

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,84 +1,103 @@
-import mongoose, {Schema} from "mongoose";
-import pkg from "jsonwebtoken";
-const {jwt} = pkg;
+import mongoose, { Schema } from "mongoose";
+// import pkg from "jsonwebtoken";
+// const { jwt } = pkg;
+import jwt from 'jsonwebtoken';
 import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique: true,
-        lowercase: true,
-        trim: true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    fullName:{
-        type:String,
-        required:true,
-        trim: true,
-        index: true
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
     },
-    avatar:{
-        type:String, // cloudinary url storing
-        required:true,
+    avatar: {
+      type: String, // cloudinary url storing
+      required: true,
     },
-    coverImage:{
-        type:String // cloudinary url storing
+    coverImage: {
+      type: String, // cloudinary url storing
     },
-    watchHistory:[
-        {
-            type:Schema.Types.ObjectId,
-            ref: "Video"
-        }
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
     ],
-    password:{
-        type:String,
-        required:[true, "Password is required"]
+    password: {
+      type: String,
+      required: [true, "Password is required"],
     },
-    refreshToken:{
-        type: String
-    }
-},{timestamps:true})
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-userSchema.pre("save",async function(next){
-    if(!this.isModified("password")) return next()
-    this.password = await bcrypt.hash(this.password, 10)
-    next();
-})
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password, this.password)
-}
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-userSchema.methods.generateAccessToken = function(){
-    jwt.sign({
+userSchema.methods.generateAccessToken = function () {
+  // console.log(process.env.ACCESS_TOKEN_SECRET, process.env.ACCESS_TOKEN_EXPIRY);
+  try {
+    return jwt.sign(
+      {
         _id: this._id,
         email: this.email,
         username: this.username,
-        fullname: this.fullname
-    },
-    process.env.ACCESS_TOKEN_SECRET,{
-        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-    }
-    )
-}
-userSchema.methods.generateRefreshToken = function(){
-    jwt.sign({
+        fullName: this.fullName,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+      }
+    );
+  } catch (error) {
+    console.log("Error in Access Token: ", error);
+    return;
+  }
+};
+userSchema.methods.generateRefreshToken = function () {
+  try {
+    return jwt.sign(
+      {
         _id: this._id,
         email: this.email,
         username: this.username,
-        fullname: this.fullname
-    },
-    process.env.REFRESH_TOKEN_SECRET,{
-        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-        }
-    )
-}
-export const User = mongoose.model("User", userSchema)
+        fullName: this.fullName,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+      }
+    );
+  } catch (error) {
+    console.log("Error in Access Token: ", error.message);
+    return;
+  }
+};
+export const User = mongoose.model("User", userSchema);
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import upload from "../middlewares/multer.middleware.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 const router  = Router()
 
@@ -15,7 +16,13 @@ router.route("/register").post(
             maxCount:1
         }
     ]),
-    registerUser)
+    registerUser
+)
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJwt ,logoutUser)
 
 
 export default router
